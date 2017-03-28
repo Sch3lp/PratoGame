4edf52f93998ca5f483c5fdffc1ea52c44bf92ec
Jacobs <Jacobs Hans@prato.be>
2017-03-28 22:55:14 +0200

    Only add > when input

--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,23 @@
 Prato.Game = function(game){
 		this.terminalHistory = ['', '', ''];
-		this.terminalHistory2 = ['WELCOME TO THE PRATO ROBBY ASSEMBLING TOOL OPERATOR', 'ALSO KNOWN AS P.R.A.T.O.', 'PLEASE REASSEMBLE ROBBY'];
+		this.arrowHistory = ['   ', '   ', ' > '];
+		this.startText = ['WELCOME TO THE PRATO ROBBY ASSEMBLING TOOL OPERATOR', 'ALSO KNOWN AS P.R.A.T.O.', 'PLEASE REASSEMBLE ROBBY'];
 		this.input;
 		this.lastSaidStuff;
+		this.otherArrows;
 };
 Prato.Game.prototype = {
 	create: function(){
-		var logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo');
-		logo.anchor.setTo(0.5, 0.5);
-		this.input = this.add.inputField(32, 550,{
-			width: 736,
+		var robby = this.add.sprite(this.world.centerX, this.world.centerY, 'robby');
+		this.add.sprite(0, 600, 'divider');
+		robby.anchor.setTo(0.5, 0.5);
+		this.input = this.add.inputField(32, 718,{
+			width: 960,
 			height: 20,
 			font: '20px Courier',
 			backgroundColor: '#000000',
-			backgroundColor: '#000000',
-			fill: '#00FF00',
-			cursorColor: '#00FF00',
+			fill: '#FFFFFF',
+			cursorColor: '#FFFFFF',
 			fillAlpha: 0,
 		});
 
@@ -24,7 +26,7 @@ Prato.Game.prototype = {
 		this.input.keyListener = function (evt) {
 			this.value = this.domElement.value;
 			if (evt.keyCode === 13) {
-				me.keyDown().bind(me);
+				me.keyDown();
 			}
 			this.updateText();
 			this.updateCursor();
@@ -32,26 +34,38 @@ Prato.Game.prototype = {
 			evt.preventDefault();
 		};
 
-		var otherArrows = this.add.text(0, 470, ' > \n > \n > ', { font: "20px Courier", fill: "#00FF00" });
-		this.lastSaidStuff = this.add.text(32, 470, this.terminalHistory.join('\n'), { font: "20px Courier", fill: "#00FF00" });
-		var typeInArrow = this.add.text(0, 550, ' > ', { font: "20px Courier", fill: "#00FF00" });
+		this.otherArrows = this.add.text(0, 638, this.arrowHistory.join('\n'), { font: "20px Courier", fill: "#FFFFFF" });
+		this.lastSaidStuff = this.add.text(32, 638, this.terminalHistory.join('\n'), { font: "20px Courier", fill: "#FFFFFF" });
+		var typeInArrow = this.add.text(0, 718, ' > ', { font: "20px Courier", fill: "#FFFFFF" });
 
-		this.typeDelayed(this.terminalHistory2.join('\n'));
+		this.typeDelayed(this.startText.join('\n'));
 	},
 	update: function () {
 	},
 	keyDown: function () {
-			var result = this.evaluateCall(this.input.value);
-			this.typeDelayed('\n' + this.input.value + '\n' + result);
-			this.input.resetText();
-			this.input.startFocus();
+		// Add new > line
+		this.arrowHistory.splice(0,1);
+		this.arrowHistory.push(' > ');
+		this.otherArrows.setText(this.arrowHistory.join('\n'))
+
+		// Throw away oldest line and provide clear new line
+		this.terminalHistory.push('');
+		this.terminalHistory.splice(0,1);
+		this.lastSaidStuff.setText(this.terminalHistory.join('\n'));
+
+		var result = this.evaluateCall(this.input.value);
+
+		this.typeDelayed(this.input.value + '\n' + result);
+		this.input.resetText();
+		this.input.startFocus();
 	},
 
 	typeDelayed: function (typeLeft) {
-			if(typeLeft[1] && typeLeft[0] === '\n'){
-						this.terminalHistory.splice(0,1);
-						this.terminalHistory.push('');
-						this.lastSaidStuff.setText(this.terminalHistory.join('\n'));
+			if(typeLeft[0] && typeLeft[0] === '\n'){
+				this.terminalHistory.splice(0,1);
+				this.terminalHistory.push('');
+				this.lastSaidStuff.setText(this.terminalHistory.join('\n'));
+				this.updateArrows();
 			} else{
 				this.lastSaidStuff.setText(this.lastSaidStuff._text + typeLeft[0]);
 				this.terminalHistory[2] += typeLeft[0];
@@ -59,8 +73,18 @@ Prato.Game.prototype = {
 			if(typeLeft.substring(1).length === 0) return;
 			this.time.events.add(25, this.typeDelayed, this, typeLeft.substring(1));
 	},
-
 	evaluateCall: function (command){
-		return '' + eval.call(this, command);
+		try {
+			return '' + eval.call(this, command);
+		}
+		catch(err) {
+			return err.message;
+		}
+	},
+	updateArrows: function (){
+		this.arrowHistory.splice(0,1);
+		this.arrowHistory.push('   ');
+		this.otherArrows.setText(this.arrowHistory.join('\n'))
+
 	}
 };
--- src/Preloader.js
+++ src/Preloader.js
@@ -1,7 +1,8 @@
 Prato.Preloader = function(game){};
 Prato.Preloader.prototype = {
 	preload: function(){
-            this.load.image('logo', 'Robby.png');
+      this.load.image('robby', 'Robby.png');
+      this.load.image('divider', 'Divider.png');
 			this.add.plugin(PhaserInput.Plugin);
 	},
 	create: function(){
